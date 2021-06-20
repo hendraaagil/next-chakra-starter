46ef6e41c4d76ff6e8a218780c4d10deab6b92f2
hendraaagil <hendraaagil@gmail.com>
2021-06-20 09:48:27 +0700

    chore(root): update dependencies

--- pages/_document.js
+++ pages/_document.js
@@ -16,7 +16,7 @@ export default class MyDocument extends NextDocument {
 
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700"
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
             rel="stylesheet"
           />
         </Head>
